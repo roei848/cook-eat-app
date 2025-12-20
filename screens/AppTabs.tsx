de1d91b6019df1ae2bd7c5424f09af01f26928c4
roeic <roeic@blackrabbit.com>
2025-12-20 22:37:35 +0200

    Add icons to tabs

--- screens/AppTabs.tsx
+++ screens/AppTabs.tsx
@@ -1,4 +1,9 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { View, StyleSheet } from "react-native";
+
+import { ThemeColors } from "../theme/colors";
+import { useThemeColors } from "../theme/useThemeColors";
 
 import HomeScreen from "../screens/rootScreens/HomeScreen";
 import SearchScreen from "../screens/rootScreens/SearchScreen";
@@ -8,14 +13,116 @@ import ProfileStack from "./rootScreens/profile/ProfileStack";
 
 const Tab = createBottomTabNavigator();
 
+type TabConfig = {
+  name: string;
+  component: React.ComponentType<any>;
+  icon: keyof typeof Ionicons.glyphMap;
+  iconOutline: keyof typeof Ionicons.glyphMap;
+  isAction?: boolean;
+};
+
 export default function AppTabs() {
+  const colors = useThemeColors();
+  const styles = createStyles(colors);
+
+  const tabs: TabConfig[] = [
+    {
+      name: "Home",
+      component: HomeScreen,
+      icon: "home",
+      iconOutline: "home-outline",
+    },
+    {
+      name: "Search",
+      component: SearchScreen,
+      icon: "search",
+      iconOutline: "search-outline",
+    },
+    {
+      name: "AddRecipe",
+      component: AddRecipeScreen,
+      icon: "add",
+      iconOutline: "add",
+      isAction: true,
+    },
+    {
+      name: "Grocery",
+      component: GroceryListScreen,
+      icon: "cart",
+      iconOutline: "cart-outline",
+    },
+    {
+      name: "Profile",
+      component: ProfileStack,
+      icon: "person",
+      iconOutline: "person-outline",
+    },
+  ];
+
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name="AddRecipe" component={AddRecipeScreen} />
-      <Tab.Screen name="Grocery" component={GroceryListScreen} />
-      <Tab.Screen name="Profile" component={ProfileStack} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: colors.primary[500],
+        tabBarInactiveTintColor: colors.text.muted,
+        tabBarStyle: styles.tabBar,
+      }}
+    >
+      {tabs.map((tab) => (
+        <Tab.Screen
+          key={tab.name}
+          name={tab.name}
+          component={tab.component}
+          options={{
+            tabBarIcon: ({ color, size, focused }) => {
+              if (tab.isAction) {
+                return (
+                  <View style={styles.addButton}>
+                    <Ionicons
+                      name="add"
+                      size={32}
+                      color={colors.text.inverse}
+                    />
+                  </View>
+                );
+              }
+
+              return (
+                <Ionicons
+                  name={focused ? tab.icon : tab.iconOutline}
+                  size={size}
+                  color={color}
+                />
+              );
+            },
+          }}
+        />
+      ))}
     </Tab.Navigator>
   );
 }
+
+const createStyles = (colors: ThemeColors) =>
+  StyleSheet.create({
+    tabBar: {
+      backgroundColor: colors.background.default,
+      borderTopColor: colors.border.default,
+      height: 64,
+      paddingBottom: 8,
+    },
+    addButton: {
+      width: 56,
+      height: 56,
+      borderRadius: 28,
+      backgroundColor: colors.primary[500],
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 24, // lifts button up
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.3,
+      shadowRadius: 4,
+      elevation: 6,
+    },
+  });
