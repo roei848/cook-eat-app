e72d1290fd61edf8912ce5aa198d5ab9dc9610a4
roeic <roeic@blackrabbit.com>
2025-12-24 22:24:58 +0200

    Enhance CategoryScreen by adding a FlatList to display recipes associated with the selected category. Refactor styles to utilize theme colors for improved consistency.

--- screens/rootScreens/search/CategoryScreen.tsx
+++ screens/rootScreens/search/CategoryScreen.tsx
@@ -5,32 +5,34 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import Screen from "../../Screen";
 import { useThemeColors } from "../../../theme/useThemeColors";
 import { SearchStackParamList } from "./SearchStack";
+import { ThemeColors } from "../../../theme/colors";
 
 type Props = NativeStackScreenProps<SearchStackParamList, "Category">;
 
 export default function CategoryScreen({ route }: Props) {
   const { category } = route.params;
   const colors = useThemeColors();
+  const styles = createStyles(colors);
 
   return (
     <Screen>
       <View style={styles.container}>
-        <Text style={[styles.title, { color: colors.text.primary }]}>
-          {category}
-        </Text>
+        <Text style={styles.title}>{category}</Text>
       </View>
     </Screen>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: "600",
-  },
-});
+const createStyles = (colors: ThemeColors) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    title: {
+      fontSize: 26,
+      fontWeight: "600",
+      color: colors.text.primary,
+    },
+  });
